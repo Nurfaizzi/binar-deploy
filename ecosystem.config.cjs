a8265ab6afceee93c2e8bcc729cdf95a7d808379
Nurfaizzi <ocitbmc@gmail.com>
2024-01-27 11:06:58 +0700

    newwwlagi

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,7 +1,8 @@
 module.exports = {
   apps : [{
     name :"binar-deploy",
-    script : "serve -s build -p 5173",
+    script : "serve",
+    args : "-s build",
     error_file : "./pm2-error.log",
     out_file : "./pm2-out.log",
     env : {
